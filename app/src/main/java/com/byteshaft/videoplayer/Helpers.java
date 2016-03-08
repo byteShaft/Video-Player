1b32753eb8e96adcddc0eaae35f85a5893a17a14
Bilal shahid <s9iper1@localhost.localdomain>
2016-03-08 17:13:02 +0500

    commit

---
+++ app/src/main/java/com/byteshaft/videoplayer/Helpers.java
@@ -0,0 +1,255 @@
+package com.byteshaft.videoplayer;
+
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.preference.PreferenceManager;
+import android.provider.MediaStore;
+import android.util.TypedValue;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Formatter;
+import java.util.Locale;
+
+public class Helpers extends ContextWrapper {
+
+    public Helpers(Context base) {
+        super(base);
+    }
+
+    float getDensityPixels(int pixels) {
+        return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, pixels, getResources().getDisplayMetrics());
+    }
+
+    boolean isVideoPortrait(double height, double width) {
+        return height > width;
+    }
+
+    WindowManager getWindowManager() {
+        return (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+    }
+
+    void destroyVideoSurface(WindowManager mWindowManager, View view) {
+        if (mWindowManager != null) {
+            mWindowManager.removeView(view);
+        }
+    }
+
+    ArrayList<String> getAllVideosUri() {
+        ArrayList<String> uris = new ArrayList<>();
+        Cursor cursor = getVideosCursor();
+        int pathColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+
+        while (cursor.moveToNext()) {
+            uris.add(cursor.getString(pathColumn));
+        }
+        cursor.close();
+        return uris;
+    }
+
+    private Cursor getVideosCursor() {
+        String[] Projection = {MediaStore.Video.Media._ID, MediaStore.Images.Media.DATA};
+        return getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                Projection, null, null, null);
+    }
+
+    String[] getVideoTitles(ArrayList<String> videos) {
+        ArrayList<String> vids = new ArrayList<>();
+        for (String video : videos) {
+            File file = new File(video);
+            vids.add(file.getName());
+        }
+        String[] realVideos = new String[vids.size()];
+        return vids.toArray(realVideos);
+    }
+
+    void playVideoForLocation(String filename, int startPosition) {
+//        Intent intent = new Intent(getApplicationContext(), VideoPlayer.class);
+//        intent.putExtra("videoUri", filename);
+//        intent.putExtra("startPosition", startPosition);
+//        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//        startActivity(intent);
+    }
+
+    void deleteFile(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media._ID};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int idColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media._ID);
+        cursor.moveToPosition(databaseIndex);
+        int id = cursor.getInt(idColumn);
+        Uri uri = ContentUris.withAppendedId(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, id);
+        getContentResolver().delete(uri, null, null);
+        cursor.close();
+    }
+
+    int getDurationForVideo(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.DURATION};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DURATION);
+        cursor.moveToPosition(databaseIndex);
+        String duration = cursor.getString(durationColumn);
+        cursor.close();
+        return Integer.valueOf(duration);
+    }
+
+    String getResolutionForVideo(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.RESOLUTION};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int resolutionColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.RESOLUTION);
+        cursor.moveToPosition(databaseIndex);
+        String resolution = cursor.getString(resolutionColumn);
+        cursor.close();
+        return resolution;
+    }
+
+    String getCreationDate(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.DATE_TAKEN};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int durationColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATE_TAKEN);
+        cursor.moveToPosition(databaseIndex);
+        String dateTaken = cursor.getString(durationColumn);
+        String CreationDate = getDate(dateTaken , "dd-MM-yyy");
+        cursor.close();
+        return CreationDate;
+    }
+
+    String getVideoAlbumName(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.ALBUM};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int albumColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.ALBUM);
+        cursor.moveToPosition(databaseIndex);
+        String dateTaken = cursor.getString(albumColumn);
+        cursor.close();
+        return dateTaken;
+    }
+
+    String getArtist(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.ARTIST};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int artistColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.ARTIST);
+        cursor.moveToPosition(databaseIndex);
+        String dateTaken = cursor.getString(artistColumn);
+        cursor.close();
+        return dateTaken;
+    }
+
+    String getVideoTitle(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.TITLE};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int titleColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.TITLE);
+        cursor.moveToPosition(databaseIndex);
+        String title = cursor.getString(titleColumn);
+        cursor.close();
+        return title;
+    }
+
+    String getLocation(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.DATA};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.DATA);
+        cursor.moveToPosition(databaseIndex);
+        String dateTaken = cursor.getString(dataColumn);
+        cursor.close();
+        return dateTaken;
+    }
+
+    String getVideoCategory(int databaseIndex) {
+        String[] projection = {MediaStore.Video.Media.CATEGORY};
+        Cursor cursor = getContentResolver().query(MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                projection, null, null, null);
+        int categoryColumn = cursor.getColumnIndexOrThrow(MediaStore.Video.VideoColumns.CATEGORY);
+        cursor.moveToPosition(databaseIndex);
+        String dateTaken = cursor.getString(categoryColumn);
+        cursor.close();
+        return dateTaken;
+    }
+
+    public static String getDate(String milliSeconds, String dateFormat)
+    {
+        // Create a DateFormatter object for displaying date in specified format.
+        SimpleDateFormat formatter = new SimpleDateFormat(dateFormat);
+
+        // Create a calendar object that will convert the date and time value in milliseconds to date.
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(Long.parseLong(milliSeconds));
+        return formatter.format(calendar.getTime());
+    }
+
+    String getFormattedTime(int timeMs) {
+        int totalSeconds = timeMs / 1000;
+        int seconds = totalSeconds % 60;
+        int minutes = (totalSeconds / 60) % 60;
+        int hours = totalSeconds / 3600;
+
+        StringBuilder mFormatBuilder = new StringBuilder();
+        Formatter mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
+
+        mFormatBuilder.setLength(0);
+        if (hours > 0) {
+            return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds).toString();
+        } else {
+            return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+        }
+    }
+
+    void setScreenBrightness(Window window, float value) {
+        WindowManager.LayoutParams layoutParams = window.getAttributes();
+        layoutParams.screenBrightness = value;
+        window.setAttributes(layoutParams);
+    }
+
+    void showLauncherHome() {
+        Intent startMain = new Intent(Intent.ACTION_MAIN);
+        startMain.addCategory(Intent.CATEGORY_HOME);
+        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(startMain);
+    }
+
+    float getCurrentBrightness(Window window) {
+        return window.getAttributes().screenBrightness;
+    }
+
+    int getCurrentVolume() {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        return am.getStreamVolume(AudioManager.STREAM_MUSIC);
+    }
+
+    void setVolume(int level) {
+        AudioManager am = (AudioManager) getSystemService(AUDIO_SERVICE);
+        am.setStreamVolume(AudioManager.STREAM_MUSIC, level, 0);
+    }
+
+    boolean isRepeatEnabled() {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        return sharedPreferences.getBoolean("repeatEnabled", false);
+    }
+
+    void setRepeatEnabled(boolean enable) {
+        SharedPreferences sharedPreferences = getPreferenceManager();
+        sharedPreferences.edit().putBoolean("repeatEnabled", enable).apply();
+    }
+
+    private SharedPreferences getPreferenceManager() {
+        return  PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+    }
+}
--- app/src/main/java/com/byteshaft/videoplayer/MainActivity.java
+++ app/src/main/java/com/byteshaft/videoplayer/MainActivity.java
@@ -1,25 +1,75 @@
 package com.byteshaft.videoplayer;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.MediaController;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        CustomVideoView.MediaPlayerStateChangedListener, MediaPlayer.OnCompletionListener {
 
     SlidingPaneLayout mSlidingPanel;
     ListView mMenuList;
+    private CustomVideoView mCustomVideoView;
+    private boolean isLandscape = true;
+    private Helpers mHelpers;
+    private Button mOverlayButton;
+    private Button mRotationButton;
+    private GestureDetectorCompat mDetector;
+    private ScreenStateListener mScreenStateListener;
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onPlaybackStateChanged(int state) {
+
+    }
+
+    @Override
+    public void onVideoViewPrepared(MediaPlayer mp) {
+
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+
+    }
+
+    private static class Screen {
+        static class Brightness {
+            static final float HIGH = 1f;
+            static final float LOW = 0f;
+        }
+    }
+
+    private static class Sound {
+        static class Level {
+            static final int MINIMUM = 0;
+            static final int MAXIMUM = 15;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        mHelpers = new Helpers(getApplicationContext());
         mSlidingPanel = (SlidingPaneLayout) findViewById(R.id.SlidingPanel);
         mMenuList = (ListView) findViewById(R.id.MenuList);
         SlidingPaneLayout.PanelSlideListener panelListener = new SlidingPaneLayout.PanelSlideListener(){
@@ -30,17 +80,29 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onPanelOpened(View arg0) {
-                // TODO Auto-generated method stub
             }
 
             @Override
             public void onPanelSlide(View arg0, float arg1) {
-                // TODO Auto-generated method stub
 
             }
         };
         mSlidingPanel.setPanelSlideListener(panelListener);
         mSlidingPanel.setParallaxDistance(300);
+        mScreenStateListener = new ScreenStateListener(mCustomVideoView);
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_OFF);
+        mDetector = new GestureDetectorCompat(this, new GestureListener());
+        mCustomVideoView = (CustomVideoView) findViewById(R.id.videoSurface);
+        mCustomVideoView.setMediaPlayerStateChangedListener(this);
+        mCustomVideoView.setOnCompletionListener(this);
+        mHelpers.setScreenBrightness(getWindow(), Screen.Brightness.HIGH);
+        CustomMediaController mediaController = new CustomMediaController(this);
+        mediaController.setAnchorView(mCustomVideoView);
+        mCustomVideoView.setMediaController(mediaController);
+        registerReceiver(mScreenStateListener, filter);
+        mCustomVideoView.setVideoPath("");
+        mCustomVideoView.seekTo(0);
+        mCustomVideoView.start();
 
     }
 
@@ -65,4 +127,91 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    class CustomMediaController extends MediaController {
+
+        public CustomMediaController(Context context) {
+            super(context);
+        }
+
+        @Override
+        public void show() {
+            super.show();
+//            mOverlayButton.setVisibility(VISIBLE);
+//            mRotationButton.setVisibility(VISIBLE);
+        }
+
+        @Override
+        public void hide() {
+            super.hide();
+//            mOverlayButton.setVisibility(INVISIBLE);
+//            mRotationButton.setVisibility(INVISIBLE);
+        }
+    }
+
+    class GestureListener extends GestureDetector.SimpleOnGestureListener {
+
+        final double BRIGHTNESS_STEP = 0.066;
+        final int VOLUME_STEP = 1;
+        private float lastTrackedPosition;
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            lastTrackedPosition = e.getY();
+            return super.onDown(e);
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            final int ACTIVITY_HEIGHT_FRAGMENT = getActivityHeight() / 50;
+            float touchX = e2.getX();
+            float touchY = e2.getY();
+            if (touchX < getActivityWidth() / 2) {
+                float brightness = mHelpers.getCurrentBrightness(getWindow());
+                if (touchY >= lastTrackedPosition + ACTIVITY_HEIGHT_FRAGMENT &&
+                        brightness - BRIGHTNESS_STEP > Screen.Brightness.LOW) {
+                    brightness -= BRIGHTNESS_STEP;
+                    mHelpers.setScreenBrightness(getWindow(), brightness);
+                    lastTrackedPosition = touchY;
+                } else if (touchY <= lastTrackedPosition - ACTIVITY_HEIGHT_FRAGMENT &&
+                        brightness + BRIGHTNESS_STEP <= Screen.Brightness.HIGH) {
+                    brightness += BRIGHTNESS_STEP;
+                    mHelpers.setScreenBrightness(getWindow(), brightness);
+                    lastTrackedPosition = touchY;
+                }
+            } else {
+                int currentVolume = mHelpers.getCurrentVolume();
+                if (touchY > lastTrackedPosition + ACTIVITY_HEIGHT_FRAGMENT &&
+                        currentVolume - VOLUME_STEP >= Sound.Level.MINIMUM) {
+                    currentVolume -= VOLUME_STEP;
+                    mHelpers.setVolume(currentVolume);
+                    lastTrackedPosition = touchY;
+                } else if (touchY <= lastTrackedPosition - ACTIVITY_HEIGHT_FRAGMENT &&
+                        currentVolume + VOLUME_STEP <= Sound.Level.MAXIMUM) {
+                    currentVolume += VOLUME_STEP;
+                    mHelpers.setVolume(currentVolume);
+                    lastTrackedPosition = touchY;
+                }
+            }
+            return super.onScroll(e1, e2, distanceX, distanceY);
+        }
+    }
+
+    private int getActivityHeight() {
+        return getWindow().getDecorView().getHeight();
+    }
+
+    private int getActivityWidth() {
+        return getWindow().getDecorView().getWidth();
+    }
+
+    private void setVideoOrientation() {
+        if (mHelpers.isVideoPortrait(mCustomVideoView.getVideoHeight(),
+                mCustomVideoView.getVideoWidth())) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+    }
 }
+
