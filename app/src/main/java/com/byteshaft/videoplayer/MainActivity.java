d9b4bbb5f94d1ba677d07653f3286c2dbabfe35f
Bilal shahid <s9iper1@localhost.localdomain>
2016-03-08 15:46:23 +0500

    final design

--- app/src/main/java/com/byteshaft/videoplayer/MainActivity.java
+++ app/src/main/java/com/byteshaft/videoplayer/MainActivity.java
@@ -1,15 +1,18 @@
 package com.byteshaft.videoplayer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.widget.SlidingPaneLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    SlidingPaneLayout mSlidingPanel;
+    ListView mMenuList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +20,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        mSlidingPanel = (SlidingPaneLayout) findViewById(R.id.SlidingPanel);
+        mMenuList = (ListView) findViewById(R.id.MenuList);
+        SlidingPaneLayout.PanelSlideListener panelListener = new SlidingPaneLayout.PanelSlideListener(){
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onPanelClosed(View arg0) {
             }
-        });
+
+            @Override
+            public void onPanelOpened(View arg0) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onPanelSlide(View arg0, float arg1) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+        mSlidingPanel.setPanelSlideListener(panelListener);
+        mSlidingPanel.setParallaxDistance(300);
+
     }
 
     @Override
